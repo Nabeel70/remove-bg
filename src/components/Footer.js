819f83fa59f72ab88d66aaabfbce94b54937d616
Nabeel70 <amin70rb@gmail.com>
2021-11-15 08:42:53 +0500

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <div>
+        <footer className="footer">
+          <div className="container">
+            <span className="text-muted">made with <i className="fa fa-heart heart"/> by</span><a href="https://my-portfolio-470.web.app/">Muhammad Nabeel Amin</a>
+          </div>
+        </footer>
+      </div>
+    );
+  }
+}
