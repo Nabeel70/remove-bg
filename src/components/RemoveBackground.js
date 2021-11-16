5e14fdfb2d8b750ec77624a38000df22c90d94ff
Muhammad Nabeel Amin <61421054+Nabeel70@users.noreply.github.com>
2021-11-16 07:45:50 +0500

    fixing errors in API

--- src/components/RemoveBackground.js
+++ src/components/RemoveBackground.js
@@ -9,6 +9,7 @@ export default function RemoveBackground() {
 
   let blob = null;
 
+
   const [image, setImage] = useState(null);
 
   const imgUpload = (e) => {
@@ -26,8 +27,8 @@ export default function RemoveBackground() {
 
     const resizedImage = await loadImage(image, {
       // resize before sending to Remove.bg for performance
-      maxWidth: 1500,
-      maxHeight: 1500,
+      maxWidth: 500,
+      maxHeight: 500,
       canvas: true,
     });
 
@@ -38,7 +39,7 @@ export default function RemoveBackground() {
       const response = await fetch("https://api.remove.bg/v1.0/removebg", {
         method: "POST",
         headers: {
-          "X-Api-Key": "Rn1PbjhV5MbZvVahFJ7jfzoh",
+          "X-Api-Key": "TXfCibpm7zU4hea1yGDRzD5Q",
         },
         body: formData,
       });
@@ -56,7 +57,7 @@ export default function RemoveBackground() {
       const down = document.getElementById("down");
       image.src = blob;
       down.href = blob;
-      down.download = "save.png";
+      down.download = "img.jpeg";
     });
   };
 
@@ -72,14 +73,14 @@ export default function RemoveBackground() {
           />
           <label
             id="upload-label"
-            for="upload"
+            htmlFor="upload"
             className="font-weight-light text-muted"
           >
             Choose file
           </label>
 
           <div className="input-group-append">
-            <label for="upload" className="btn btn-light m-0 rounded-pill px-4">
+            <label htmlFor="upload" className="btn btn-light m-0 rounded-pill px-4">
               {" "}
               <i className="fa fa-cloud-upload mr-2 text-muted"></i>
               <small className="text-uppercase font-weight-bold text-muted">
@@ -115,19 +116,20 @@ export default function RemoveBackground() {
                 ) : (
                   <img
                     id="imageResult"
-                    src="#"
+                    src="/#"
                     alt=""
                     className="img-fluid rounded shadow-sm mx-auto d-block"
                   />
                 )}{" "}
               </div>
               {status ? (
-                <a id="down">
+                   // eslint-disable-next-line jsx-a11y/anchor-is-valid
+                   <a id="down">
                   <button className="btn btn-light down-button">
                     {" "}
                     <i className="fas fa-download" /> Download
                   </button>
-                </a>
+                  </a>
               ) : null}
             </div>
           </div>
