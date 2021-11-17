976e302ca2464d64183432554f4e30675d0dfad2
Nabeel70 <amin70rb@gmail.com>
2021-11-17 09:19:15 +0500

    --- Auto-Git Commit ---

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,58 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { Component } from "react";
 
 export default class Footer extends Component {
   render() {
     return (
       <div>
+        <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
         <footer className="footer">
-          <div className="container">
-            <span className="text-muted">made with <i className="fa fa-heart heart"/> by</span><a href="https://my-portfolio-470.web.app/">Muhammad Nabeel Amin</a>
-          </div>
+          <br></br> 
+  <ul class="footer__nav">
+    <li class="nav__item">
+      <h2 class="nav__title">Media</h2>
+
+      <ul class="nav__ul">
+        <li>
+          <a href="#">Online</a>
+        </li>
+
+        <li>
+          <a href="#">Print</a>
+        </li>
+            
+        <li>
+          <a href="#">Alternative Ads</a>
+        </li>
+      </ul>
+    </li>
+    
+    <li class="nav__item">
+      <h2 class="nav__title">Legal</h2>
+      
+      <ul class="nav__ul">
+        <li>
+          <a href="/#">Privacy Policy</a>
+        </li>
+        
+        <li>
+          <a href="#">Terms of Use</a>
+        </li>
+        
+        <li>
+          <a href="#">Sitemap</a>
+        </li>
+      </ul>
+    </li>
+  </ul>
+  
+  <div class="legal">
+    <p>&copy; 2021 RemoveAI. All rights reserved.</p>
+    
+    <div class="legal__links">
+      <a href="http://www.removeai.com/">Made by removeai.com</a> 
+    </div>
+  </div>
         </footer>
       </div>
     );
