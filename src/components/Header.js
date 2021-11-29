39d197b271c36ae97ba1ed0fd6f7a3f74f5896e9
Nabeel70 <amin70rb@gmail.com>
2021-11-29 11:55:23 +0500

    --- Auto-Git Commit ---

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,27 @@
 import React, { Component } from "react";
-import { Navbar,Nav, Container, NavDropdown, Form, FormControl, Button, } from 'react-bootstrap'
-import './extra.css'
+import { Navbar,Nav, Container} from 'react-bootstrap';
+import './extra.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+
+
+
+
 export default class Header extends Component {
   render() {
     return (
       <div>
 
                    {/* Navbar code */}
-
+<Router>
 <Navbar id="navibar" bg="light" expand="lg">
   <Container fluid>
-    <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+    <Navbar.Brand className="nav__title" as={Link} to={"/home"}>Background Removal</Navbar.Brand>
     <Navbar.Toggle aria-controls="navbarScroll" />
     <Navbar.Collapse id="navbarScroll">
       <Nav
@@ -18,32 +29,29 @@ export default class Header extends Component {
         style={{ maxHeight: '100px' }}
         navbarScroll
       >
-        <Nav.Link href="#action1">Home</Nav.Link>
-        <Nav.Link href="#action2">Link</Nav.Link>
-        <NavDropdown title="Link" id="navbarScrollingDropdown">
-          <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-          <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
-          <NavDropdown.Divider />
-          <NavDropdown.Item href="#action5">
-            Something else here
-          </NavDropdown.Item>
-        </NavDropdown>
-        <Nav.Link href="#" disabled>
-          Link
-        </Nav.Link>
+    
       </Nav>
-      <Form className="d-flex">
-        <FormControl
-          type="search"
-          placeholder="Search"
-          className="me-2"
-          aria-label="Search"
-        />
-        <Button variant="outline-success">Search</Button>
-      </Form>
+      <Nav className="nav__title">
+      <Nav.Link as={Link} to={"/privacy-policy"}>Privacy Policy</Nav.Link>
+        <Nav.Link as={Link} to={"/contact-us"}>Contact-us</Nav.Link>
+        </Nav>
     </Navbar.Collapse>
   </Container>
 </Navbar>
+<div>
+<Switch>
+          <Route path="/">
+            <home/>
+          </Route>
+          <Route path="/privacy-policy">
+            <privacy-policy />
+          </Route>
+          <Route path="/contact-us">
+            <contact-us />
+          </Route>
+        </Switch>
+</div>
+</Router>
 
 
 
