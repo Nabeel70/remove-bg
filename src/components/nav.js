8ecf4d69b6f71bd931d24ea96b0af07a9cf4d8cb
Nabeel70 <amin70rb@gmail.com>
2021-11-29 13:37:38 +0500

    --- Auto-Git Commit ---

--- src/components/nav.js
+++ src/components/nav.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import { Navbar,Nav, Container } from 'react-bootstrap';
 import './extra.css';
 import './RemoveBackground.js';
-import './nav'
 import {
   BrowserRouter as Router,
   Route,
@@ -15,15 +14,12 @@ import ContactUs from './contact';
 
 
 
-export default class Header extends Component {
-  render() {
-    return (
-      <div>
-
+                   // eslint-disable-next-line no-lone-blocks
                    {/* Navbar code */}
 <Router>
 <Navbar id="navibar" bg="light" expand="lg">
   <Container fluid>
+  <Link to="/home" className="btn btn-primary">hello</Link>
     <Navbar.Brand className="nav__title" as={Link} to={"/home"}>Background Removal</Navbar.Brand>
     <Navbar.Toggle aria-controls="navbarScroll" />
     <Navbar.Collapse id="navbarScroll">
@@ -50,17 +46,4 @@ export default class Header extends Component {
          
         </Routes>    
 </div>
-</Router>        <header className="text-white text-center">
-          <img
-            alt="Logo pic"
-            src="https://i.ibb.co/N9nkZRH/img.png"
-            width="120"
-            className="mb-4"
-          />
-          <h1 className="display-4">Background Removal Web App</h1>
-          <p className="lead mb-0">Fast, simple and great performance.</p>
-        </header>
-      </div>
-    );
-  }
-}
+</Router>
