f5857433cab497da5b44c80d33165314f9bc6076
Nabeel70 <amin70rb@gmail.com>
2021-11-30 10:56:30 +0500

    --- Auto-Git Commit ---

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,6 @@
 import React, { Component } from "react";
-import { Navbar,Nav, Container } from 'react-bootstrap';
 import './extra.css';
 import './RemoveBackground.js';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import Home from "./RemoveBackground.js";
-import Privacy from "./Privacy";
-import ContactUs from './contact'
-
 
 
 
@@ -19,39 +8,6 @@ export default class Header extends Component {
   render() {
     return (
       <div>
-
-                   {/* Navbar code */}
-
-<Router>
-<Navbar id="navibar" bg="light" expand="lg">
-  <Container fluid>
-    <Navbar.Brand className="nav__title" as={Link} to={"/home"}>Background Removal</Navbar.Brand>
-    <Navbar.Toggle aria-controls="navbarScroll" />
-    <Navbar.Collapse id="navbarScroll">
-      <Nav
-        className="me-auto my-2 my-lg-0"
-        style={{ maxHeight: '100px' }}
-        navbarScroll
-      >
-    
-      </Nav>
-      <Nav className="nav__title">
-      <Nav.Link as={Link} to={"/Privacy"}>Privacy Policy</Nav.Link>
-        <Nav.Link as={Link} to={"/contactUs"}>Contact-us</Nav.Link>
-        </Nav>
-    </Navbar.Collapse>
-  </Container>
-</Navbar>
-
-<Switch>
-<div>
-<Route path="/home"> <Home /> </Route>
-<Route path="/Privacy"> <Privacy/></Route>
-<Route path="/contactUs"> <ContactUs/></Route>
-  
-</div>
-</Switch>
-</Router>
 
         <header className="text-white text-center">
           <img
